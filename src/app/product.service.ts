a964e3d6a5085b698207976eb6655d722b4cf576
Manish Jarugu <majarugu@microsoft.com>
2018-04-27 16:07:10 -0700

    module 2 updated

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  _albumUrl: string ='../assets/album.json';
+  private _albumUrl ='../assets/album.json';
 
   constructor(private _http: Http) { }
 
